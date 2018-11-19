238b7698da8f9e2db2ac84805c9c27ff46a0dadf
bcalou <bastien.calou@gmail.com>
2018-11-19 15:29:33 +0100

    [js] improve html formatting

--- src/js/utils/code.ts
+++ src/js/utils/code.ts
@@ -18,6 +18,7 @@ function formatHtml(code: string): string {
   let source = Array.from(code);
   let formatted = '';
   let indentLevel = 0;
+  let tree: string[] = [];
 
   source.forEach((char, i) => {
     if (char === '<') {
@@ -25,19 +26,29 @@ function formatHtml(code: string): string {
         .slice(i + 1, i + source.slice(i).indexOf('>'))
         .join('');
 
-      if (tag.slice(-1) !== '/' && tag[0] !== '/') {
-        indentLevel++;
+      const tagName: string = tag.split(' ')[0];
+      const closing = tagName[0] === '/';
+      const selfClosing = !closing && tag.indexOf('/') > -1;
+
+      // Remove tag from tree when closing
+      if (closing) {
+        tree.splice(tree.lastIndexOf(tagName.replace('/', '')), 1);
       }
 
       // Add indent spaces depending on nesting level
-      if (indentLevel > 0 && source[i - 1] === '>') {
-        for (let j = 1; j < indentLevel; j++) {
+      if (
+        tree.length > 0 &&
+        source[i - 1] === '>' &&
+        !((closing || selfClosing) && source[i - 1] !== '>')
+      ) {
+        for (let j = 0; j < tree.length; j++) {
           formatted += '&nbsp;&nbsp;';
         }
       }
 
-      if (tag.slice(-1) !== '/' && tag[0] === '/') {
-        indentLevel--;
+      // Add tag to tree if opening
+      if (!selfClosing && !closing) {
+        tree.push(tagName);
       }
     }
 
@@ -47,7 +58,7 @@ function formatHtml(code: string): string {
   return formatted
     .replace(/</g, '&lt;') // Encode <
     .replace(/>/g, '&gt;') // Encode >
-    .replace(/&gt;&lt;/g, '&gt;<br/>&lt;') // Add line break after each tag;
+    .replace(/&gt;&lt;/g, '&gt;<br/>&lt;') // Add line break after each tag
     .replace(/&gt;&nbsp;/g, '&gt;<br/>&nbsp;'); // When a closing tag is followed by space, add line break
 }
 
